c05c889a85da9a17d14cdaa191e82e594ec4fcaf
mmmim24 <mustaq24@student.sust.edu>
2024-03-25 20:44:31 +0600

    mongo URI

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const userRoute = require('./routes/userRoute');
 const complaintRoute = require('./routes/complaintRoute');
 
 const PORT = process.env.PORT || 3333
-const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/shpc'
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/shppc'
 
 const app = express()
 
